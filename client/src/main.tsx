19e6976f0d97f698956bbde0d1df3e2fd603d26d
Daniel de Andrade Varela <1902294+drachehavoc@users.noreply.github.com>
2023-08-01 13:01:28 +0000

    ...

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,11 @@
+import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root')!
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
---
+++ client/src/templates/Login.tsx
@@ -0,0 +1,14 @@
+export default () => <>
+    <form>
+        <div>
+            <label>Login</label>
+            <input name="login" />
+        </div>
+        <div>
+            <label>Senha</label>
+            <input type="password" name="password" />
+        </div>
+        <button>entrar</button>
+        <button>recuperar senha</button>
+    </form>
+</>
